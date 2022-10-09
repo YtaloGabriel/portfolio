603779f0f66d227ea7a5e04ec316ddf9888d6331
ytalogabr <ytalogabr@gmail.com>
2022-10-09 04:52:10 -0300

    updating projects links

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -12,16 +12,23 @@ const Projects = () => {
     <div className={`${Styles.content} slideToRight`}>
       <TitleContent colorTheme="titleTheme2">Projetos Favoritos</TitleContent>
       <div className={Styles.cards}>
-        <ProjectCard title="Dogs" subtitle="Rede Social" image={dogs} />
+        <ProjectCard
+          title="Dogs"
+          subtitle="Rede Social"
+          image={dogs}
+          link="https://ytalogabriel.github.io/dogs/"
+        />
         <ProjectCard
           title="The Weather"
           subtitle="Informações do tempo/clima"
           image={theweather}
+          link="https://ytalogabriel.github.io/the-weather/"
         />
         <ProjectCard
           title="Ulver"
           subtitle="Landing page Freelancer"
           image={ulver}
+          link="https://ytalogabriel.github.io/ulver-project/"
         />
       </div>
       <a
